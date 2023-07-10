7aa59de7c774b1efd66970b67cf5c9870020e3db
BlueXT <bluextchannel@gmail.com>
2023-07-10 15:08:10 +0300

    Добавил сущность статистики устройства

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Domain/DeviceStatistics/DeviceStatistic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace BlueXT.MobileMonitoring.DeviceStatistics;
+
+/// <summary>
+/// Статистика устройства.
+/// </summary>
+public class DeviceStatistic : FullAuditedEntity<Guid>
+{
+    /// <summary>
+    /// Id устройства.
+    /// </summary>
+    public Guid DeviceId { get; set; }
+
+    /// <summary>
+    /// Имя пользователя.
+    /// </summary>
+    public string Username { get; set; }
+
+    /// <summary>
+    /// Операционная система.
+    /// </summary>
+    public string OperatingSystem { get; set; }
+
+    /// <summary>
+    /// Версия установленного приложения.
+    /// </summary>
+    public string AppVersion { get; set; }
+}
