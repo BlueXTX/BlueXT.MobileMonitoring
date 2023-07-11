56dd150bb13fbe26a573e37958143c0cb3273bac
BlueXT <bluextchannel@gmail.com>
2023-07-11 15:33:21 +0300

    Рефактор модуля AuthServer

--- aspnet-core/src/BlueXT.MobileMonitoring.AuthServer/MobileMonitoringAuthServerModule.cs
+++ aspnet-core/src/BlueXT.MobileMonitoring.AuthServer/MobileMonitoringAuthServerModule.cs
@@ -9,6 +9,8 @@ using Medallion.Threading.Redis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using StackExchange.Redis;
@@ -50,6 +52,29 @@ namespace BlueXT.MobileMonitoring;
 public class MobileMonitoringAuthServerModule : AbpModule
 {
     /// <summary>
+    /// Конфигурация сервисов.
+    /// </summary>
+    /// <param name="context">Контекст конфигурации.</param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+
+        ConfigureLocalizationOptions();
+        ConfigureBundlingOptions();
+        ConfigureAuditingOptions();
+        ConfigureUrls(configuration);
+        ConfigureBackgroundJobOptions();
+        ConfigureDistributedCacheOptions();
+        ConfigureCors(context, configuration);
+
+        if (hostingEnvironment.IsDevelopment()) ReplaceEmbeddedFilesByPhysical(hostingEnvironment);
+
+        ConfigureDataProtection(context, hostingEnvironment, configuration);
+        ConfigureDistributedLockProvider(context, configuration);
+    }
+
+    /// <summary>
     /// Предварительная конфигурация сервисов.
     /// </summary>
     /// <param name="context">Контекст конфигурации.</param>
@@ -65,113 +90,6 @@ public class MobileMonitoringAuthServerModule : AbpModule
                         options.UseAspNetCore();
                     });
             });
-
-    /// <summary>
-    /// Конфигурация сервисов.
-    /// </summary>
-    /// <param name="context">Контекст конфигурации.</param>
-    public override void ConfigureServices(ServiceConfigurationContext context)
-    {
-        var hostingEnvironment = context.Services.GetHostingEnvironment();
-        var configuration = context.Services.GetConfiguration();
-
-        Configure<AbpLocalizationOptions>(
-            options =>
-            {
-                options.Resources
-                    .Get<MobileMonitoringResource>()
-                    .AddBaseTypes(
-                        typeof(AbpUiResource)
-                    );
-            });
-
-        Configure<AbpBundlingOptions>(
-            options =>
-            {
-                options.StyleBundles.Configure(
-                    LeptonXLiteThemeBundles.Styles.Global,
-                    bundle =>
-                    {
-                        bundle.AddFiles("/global-styles.css");
-                    }
-                );
-            });
-
-        Configure<AbpAuditingOptions>(
-            options =>
-            {
-                //options.IsEnabledForGetRequests = true;
-                options.ApplicationName = "AuthServer";
-            });
-
-        if (hostingEnvironment.IsDevelopment())
-        {
-            Configure<AbpVirtualFileSystemOptions>(
-                options =>
-                {
-                    options.FileSets.ReplaceEmbeddedByPhysical<MobileMonitoringDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}BlueXT.MobileMonitoring.Domain.Shared"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<MobileMonitoringDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}BlueXT.MobileMonitoring.Domain"));
-                });
-        }
-
-        Configure<AppUrlOptions>(
-            options =>
-            {
-                options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
-                options.RedirectAllowedUrls.AddRange(configuration["App:RedirectAllowedUrls"]?.Split(separator: ',') ?? Array.Empty<string>());
-
-                options.Applications["Angular"].RootUrl = configuration["App:ClientUrl"];
-                options.Applications["Angular"].Urls[AccountUrlNames.PasswordReset] = "account/reset-password";
-            });
-
-        Configure<AbpBackgroundJobOptions>(
-            options =>
-            {
-                options.IsJobExecutionEnabled = false;
-            });
-
-        Configure<AbpDistributedCacheOptions>(
-            options =>
-            {
-                options.KeyPrefix = "MobileMonitoring:";
-            });
-
-        var dataProtectionBuilder = context.Services.AddDataProtection().SetApplicationName("MobileMonitoring");
-        if (!hostingEnvironment.IsDevelopment())
-        {
-            var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
-            dataProtectionBuilder.PersistKeysToStackExchangeRedis(redis, "MobileMonitoring-Protection-Keys");
-        }
-
-        context.Services.AddSingleton<IDistributedLockProvider>(
-            _ =>
-            {
-                var connection = ConnectionMultiplexer
-                    .Connect(configuration["Redis:Configuration"]);
-                return new RedisDistributedSynchronizationProvider(connection.GetDatabase());
-            });
-
-        context.Services.AddCors(
-            options =>
-            {
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder
-                            .WithOrigins(
-                                configuration["App:CorsOrigins"]?
-                                    .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                                    .Select(o => o.RemovePostFix("/"))
-                                    .ToArray() ?? Array.Empty<string>()
-                            )
-                            .WithAbpExposedHeaders()
-                            .SetIsOriginAllowedToAllowWildcardSubdomains()
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .AllowCredentials();
-                    });
-            });
-    }
 
     /// <summary>
     /// Инициализация приложения.
@@ -207,4 +125,112 @@ public class MobileMonitoringAuthServerModule : AbpModule
         app.UseAbpSerilogEnrichers();
         app.UseConfiguredEndpoints();
     }
+
+    private static void ConfigureDataProtection(
+        ServiceConfigurationContext context,
+        IWebHostEnvironment hostingEnvironment,
+        IConfiguration configuration)
+    {
+        var dataProtectionBuilder = ConfigureApplicationName(context);
+        if (hostingEnvironment.IsDevelopment()) return;
+        
+        var redisConnection = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
+        dataProtectionBuilder.PersistKeysToStackExchangeRedis(redisConnection, "MobileMonitoring-Protection-Keys");
+    }
+
+    private static IDataProtectionBuilder ConfigureApplicationName(ServiceConfigurationContext context) => context.Services.AddDataProtection().SetApplicationName("MobileMonitoring");
+
+    private static void ConfigureDistributedLockProvider(ServiceConfigurationContext context, IConfiguration configuration) =>
+        context.Services.AddSingleton<IDistributedLockProvider>(
+            _ =>
+            {
+                var connection = ConnectionMultiplexer
+                    .Connect(configuration["Redis:Configuration"]);
+                return new RedisDistributedSynchronizationProvider(connection.GetDatabase());
+            });
+
+    private static void ConfigureCors(ServiceConfigurationContext context, IConfiguration configuration) =>
+        context.Services.AddCors(
+            options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder
+                            .WithOrigins(
+                                configuration["App:CorsOrigins"]?
+                                    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(o => o.RemovePostFix("/"))
+                                    .ToArray() ?? Array.Empty<string>()
+                            )
+                            .WithAbpExposedHeaders()
+                            .SetIsOriginAllowedToAllowWildcardSubdomains()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+            });
+
+    private void ReplaceEmbeddedFilesByPhysical(IWebHostEnvironment hostingEnvironment) =>
+        Configure<AbpVirtualFileSystemOptions>(
+            options =>
+            {
+                options.FileSets.ReplaceEmbeddedByPhysical<MobileMonitoringDomainSharedModule>(
+                    Path.Combine(
+                        hostingEnvironment.ContentRootPath,
+                        $"..{Path.DirectorySeparatorChar}BlueXT.MobileMonitoring.Domain.Shared"));
+                options.FileSets.ReplaceEmbeddedByPhysical<MobileMonitoringDomainModule>(
+                    Path.Combine(
+                        hostingEnvironment.ContentRootPath,
+                        $"..{Path.DirectorySeparatorChar}BlueXT.MobileMonitoring.Domain"));
+            });
+
+    private void ConfigureDistributedCacheOptions() =>
+        Configure<AbpDistributedCacheOptions>(
+            options => { options.KeyPrefix = "MobileMonitoring:"; });
+
+    private void ConfigureBackgroundJobOptions() =>
+        Configure<AbpBackgroundJobOptions>(
+            options => { options.IsJobExecutionEnabled = false; });
+
+    private void ConfigureUrls(IConfiguration configuration) =>
+        Configure<AppUrlOptions>(
+            options =>
+            {
+                options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
+                options.RedirectAllowedUrls.AddRange(
+                    configuration["App:RedirectAllowedUrls"]?.Split(separator: ',') ??
+                    Array.Empty<string>());
+
+                options.Applications["Angular"].RootUrl = configuration["App:ClientUrl"];
+                options.Applications["Angular"].Urls[AccountUrlNames.PasswordReset] = "account/reset-password";
+            });
+
+    private void ConfigureAuditingOptions() =>
+        Configure<AbpAuditingOptions>(
+            options =>
+            {
+                options.ApplicationName = "AuthServer";
+            });
+
+    private void ConfigureBundlingOptions() =>
+        Configure<AbpBundlingOptions>(
+            options =>
+            {
+                options.StyleBundles.Configure(
+                    LeptonXLiteThemeBundles.Styles.Global,
+                    bundle => { bundle.AddFiles("/global-styles.css"); }
+                );
+            });
+
+    private void ConfigureLocalizationOptions() =>
+        Configure<AbpLocalizationOptions>(
+            options =>
+            {
+                options.Resources
+                    .Get<MobileMonitoringResource>()
+                    .AddBaseTypes(
+                        typeof(AbpUiResource)
+                    );
+            });
 }
