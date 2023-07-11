4973c83744dd9fb19d4446beb09e864c5f450894
BlueXT <bluextchannel@gmail.com>
2023-07-11 17:35:33 +0300

    Сделал Full-Cleanup

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,24 +3,24 @@ import { Environment } from '@abp/ng.core';
 const baseUrl = 'http://localhost:4200';
 
 export const environment = {
-  production: true,
-  application: {
-    baseUrl,
-    name: 'MobileMonitoring',
-    logoUrl: '',
-  },
-  oAuthConfig: {
-    issuer: 'https://localhost:44323/',
-    redirectUri: baseUrl,
-    clientId: 'MobileMonitoring_App',
-    responseType: 'code',
-    scope: 'offline_access MobileMonitoring',
-    requireHttps: true
-  },
-  apis: {
-    default: {
-      url: 'https://localhost:44391',
-      rootNamespace: 'BlueXT.MobileMonitoring',
+    production: true,
+    application: {
+        baseUrl,
+        name: 'MobileMonitoring',
+        logoUrl: '',
     },
-  },
+    oAuthConfig: {
+        issuer: 'https://localhost:44323/',
+        redirectUri: baseUrl,
+        clientId: 'MobileMonitoring_App',
+        responseType: 'code',
+        scope: 'offline_access MobileMonitoring',
+        requireHttps: true,
+    },
+    apis: {
+        default: {
+            url: 'https://localhost:44391',
+            rootNamespace: 'BlueXT.MobileMonitoring',
+        },
+    },
 } as Environment;
