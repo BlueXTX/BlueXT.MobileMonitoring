dd11da521b7bd9ffcd3639791c1270a2fc2604b0
BlueXT <bluextchannel@gmail.com>
2023-07-12 10:25:26 +0300

    Реализовал компонент device-statistics и добавил локализацию для него

--- angular/src/app/device-statistics/device-statistics.module.ts
+++ angular/src/app/device-statistics/device-statistics.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DeviceStatisticsRoutingModule } from './device-statistics-routing.module';
 import { DeviceStatisticsComponent } from './device-statistics.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [DeviceStatisticsComponent],
-    imports: [CommonModule, DeviceStatisticsRoutingModule],
+    imports: [CommonModule, DeviceStatisticsRoutingModule, SharedModule],
 })
 export class DeviceStatisticsModule {}
