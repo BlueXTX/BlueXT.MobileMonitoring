5f925b335bf1e6f5e493a2e786d218bcee060b65
BlueXT <bluextchannel@gmail.com>
2023-07-13 11:13:12 +0300

    Починил генерацию документации redocly

--- aspnet-core/src/BlueXT.MobileMonitoring.HttpApi.Host/generate-docs.sh
+++ aspnet-core/src/BlueXT.MobileMonitoring.HttpApi.Host/generate-docs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-npx @redocly/cli build-docs https://localhost:44391/swagger/v1/swagger.json
+mkdir -p docs
+curl https://localhost:44391/swagger/v1/swagger.json -o ./docs/specification.json
+npx @redocly/cli build-docs ./docs/specification.json -o ./docs/redoc-static.html
