43f115f38854bbc40b3fcbf398377dcf62cbc46e
BlueXT <bluextchannel@gmail.com>
2023-07-13 14:37:29 +0300

    Определил интерфейс dapper репозитория для работы с DeviceStatistic

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Domain/DeviceStatistics/IDeviceStatisticRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Volo.Abp.Domain.Repositories;
+
+namespace BlueXT.MobileMonitoring.DeviceStatistics;
+
+/// <summary>
+/// Интерфейс базового репозитория для работы с <see cref="DeviceStatistic"/>.
+/// </summary>
+public interface IDeviceStatisticRepository : IBasicRepository<DeviceStatistic, Guid>
+{
+}
