4bc33d840247973e7c2aaf853c77238d91f94efc
BlueXT <bluextchannel@gmail.com>
2023-07-14 12:08:08 +0300

    Реализовал создание и получение DeviceStatistic через Dapper

--- aspnet-core/src/BlueXT.MobileMonitoring.Application/DeviceStatistics/DeviceStatisticService.cs
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application/DeviceStatistics/DeviceStatisticService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.ObjectMapping;
 
 namespace BlueXT.MobileMonitoring.DeviceStatistics;
 
@@ -10,12 +13,36 @@ namespace BlueXT.MobileMonitoring.DeviceStatistics;
 /// </summary>
 public class DeviceStatisticService : CrudAppService<DeviceStatistic, DeviceStatisticDto, Guid, PagedAndSortedResultRequestDto, CreateOrUpdateDeviceStatisticDto>, IDeviceStatisticService
 {
+    private readonly IObjectMapper _mapper;
+    private readonly IDeviceStatisticRepository _repository;
+
     /// <summary>
     /// Конструктор класса.
     /// </summary>
-    /// <param name="repository">Репозиторий.</param>
-    public DeviceStatisticService(IRepository<DeviceStatistic, Guid> repository)
-        : base(repository)
+    /// <param name="baseRepository">Репозиторий.</param>
+    public DeviceStatisticService(
+        IRepository<DeviceStatistic, Guid> baseRepository,
+        IObjectMapper mapper,
+        IDeviceStatisticRepository repository)
+        : base(baseRepository)
     {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public override async Task<DeviceStatisticDto> CreateAsync(CreateOrUpdateDeviceStatisticDto input)
+    {
+        var entity = _mapper.Map<CreateOrUpdateDeviceStatisticDto, DeviceStatistic>(input);
+        return _mapper.Map<DeviceStatistic, DeviceStatisticDto>(await _repository.InsertAsync(entity));
+    }
+
+    public override async Task<DeviceStatisticDto> GetAsync(Guid id) => _mapper.Map<DeviceStatistic, DeviceStatisticDto>(await _repository.GetAsync(id));
+
+    public override async Task<PagedResultDto<DeviceStatisticDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+    {
+        var totalCount = await _repository.GetCountAsync();
+        var entities = await _repository.GetPagedListAsync(input.SkipCount, input.MaxResultCount, input.Sorting);
+        var mappedEntities = _mapper.Map<List<DeviceStatistic>, List<DeviceStatisticDto>>(entities);
+        return new PagedResultDto<DeviceStatisticDto>(totalCount, mappedEntities);
     }
 }
--- aspnet-core/src/BlueXT.MobileMonitoring.Dapper/Repositories/DapperDeviceStatisticRepository.cs
+++ aspnet-core/src/BlueXT.MobileMonitoring.Dapper/Repositories/DapperDeviceStatisticRepository.cs
@@ -1,31 +1,71 @@
-﻿using BlueXT.MobileMonitoring.DeviceStatistics;
+using BlueXT.MobileMonitoring.DeviceStatistics;
 using BlueXT.MobileMonitoring.EntityFrameworkCore;
+using Dapper;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Repositories.Dapper;
 using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Guids;
+using Volo.Abp.Timing;
 
 namespace BlueXT.MobileMonitoring.Dapper.Repositories;
 
 public class DapperDeviceStatisticRepository : DapperRepository<MobileMonitoringDbContext>, IDeviceStatisticRepository, ITransientDependency
 {
-    public DapperDeviceStatisticRepository(IDbContextProvider<MobileMonitoringDbContext> dbContextProvider)
+    private readonly IGuidGenerator _guidGenerator;
+    private readonly IClock _clock;
+
+    public DapperDeviceStatisticRepository(IDbContextProvider<MobileMonitoringDbContext> dbContextProvider, IGuidGenerator guidGenerator, IClock clock)
         : base(dbContextProvider)
     {
+        _guidGenerator = guidGenerator;
+        _clock = clock;
     }
 
     public Task<List<DeviceStatistic>> GetListAsync(bool includeDetails = false, CancellationToken cancellationToken = new()) => throw new NotImplementedException();
 
-    public Task<long> GetCountAsync(CancellationToken cancellationToken = new()) => throw new NotImplementedException();
+    public async Task<long> GetCountAsync(CancellationToken cancellationToken = new())
+    {
+        const string Sql = "SELECT COUNT(*) FROM app_device_statistic WHERE is_deleted = false";
+        var connection = await GetDbConnectionAsync();
+        return await connection.ExecuteScalarAsync<int>(Sql, await GetDbTransactionAsync());
+    }
 
-    public Task<List<DeviceStatistic>> GetPagedListAsync(
+    public async Task<List<DeviceStatistic>> GetPagedListAsync(
         int skipCount,
         int maxResultCount,
         string sorting,
         bool includeDetails = false,
-        CancellationToken cancellationToken = new()) =>
-        throw new NotImplementedException();
+        CancellationToken cancellationToken = new())
+    {
+        const string Sql = @"SELECT * FROM app_device_statistic WHERE is_deleted = false OFFSET @Offset LIMIT @Limit;";
+        var connection = await GetDbConnectionAsync();
+        return (await connection.QueryAsync<DeviceStatistic>(
+            Sql,
+            new
+            {
+                Offset = skipCount,
+                Limit = maxResultCount,
+            },
+            await GetDbTransactionAsync())).ToList();
+    }
 
-    public Task<DeviceStatistic> InsertAsync(DeviceStatistic entity, bool autoSave = false, CancellationToken cancellationToken = new()) => throw new NotImplementedException();
+    public async Task<DeviceStatistic> InsertAsync(DeviceStatistic entity, bool autoSave = false, CancellationToken cancellationToken = new())
+    {
+        const string Sql = @"INSERT INTO app_device_statistic (id, device_id, username, operating_system, app_version, creation_time) VALUES (@Id, @DeviceId, @Username, @OperatingSystem, @AppVersion, @CreationTime) RETURNING *;";
+        var connection = await GetDbConnectionAsync();
+        return await connection.QuerySingleAsync<DeviceStatistic>(
+            Sql,
+            new
+            {
+                Id = _guidGenerator.Create(),
+                DeviceId = entity.DeviceId,
+                Username = entity.Username,
+                OperatingSystem = entity.OperatingSystem,
+                AppVersion = entity.AppVersion,
+                CreationTime = _clock.Now,
+            },
+            await GetDbTransactionAsync());
+    }
 
     public Task InsertManyAsync(IEnumerable<DeviceStatistic> entities, bool autoSave = false, CancellationToken cancellationToken = new()) => throw new NotImplementedException();
 
@@ -37,7 +77,12 @@ public class DapperDeviceStatisticRepository : DapperRepository<MobileMonitoring
 
     public Task DeleteManyAsync(IEnumerable<DeviceStatistic> entities, bool autoSave = false, CancellationToken cancellationToken = new()) => throw new NotImplementedException();
 
-    public Task<DeviceStatistic> GetAsync(Guid id, bool includeDetails = true, CancellationToken cancellationToken = new()) => throw new NotImplementedException();
+    public async Task<DeviceStatistic> GetAsync(Guid id, bool includeDetails = true, CancellationToken cancellationToken = new())
+    {
+        const string Sql = "SELECT * FROM app_device_statistic WHERE id = @Id";
+        var connection = await GetDbConnectionAsync();
+        return await connection.QuerySingleAsync<DeviceStatistic>(Sql, new { Id = id }, await GetDbTransactionAsync());
+    }
 
     public Task<DeviceStatistic> FindAsync(Guid id, bool includeDetails = true, CancellationToken cancellationToken = new()) => throw new NotImplementedException();
 
