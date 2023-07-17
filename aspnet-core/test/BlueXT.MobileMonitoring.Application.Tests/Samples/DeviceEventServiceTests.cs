3ca7fbc59543ff2ee86a585b38cf2c6a14cd0dd2
BlueXT <bluextchannel@gmail.com>
2023-07-17 12:07:01 +0300

    Добавил тесты для DeviceEventService

---
+++ aspnet-core/test/BlueXT.MobileMonitoring.Application.Tests/Samples/DeviceEventServiceTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlueXT.MobileMonitoring.DeviceEvents;
+using Bogus;
+using Shouldly;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace BlueXT.MobileMonitoring.Samples;
+
+public sealed class DeviceEventServiceTests : MobileMonitoringApplicationTestBase
+{
+    private readonly Faker _faker = new();
+    private readonly DeviceEventService _deviceEventService;
+
+    public DeviceEventServiceTests() => _deviceEventService = GetRequiredService<DeviceEventService>();
+
+    [Fact]
+    public async Task CreateAsync_WithValidDto_ShouldReturnEntity()
+    {
+        var createdEntity = await CreateEntity();
+
+        createdEntity.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async Task CreateAsync_WithInvalidDto_ShouldThrow()
+    {
+        var dto = new CreateOrUpdateDeviceEventDto
+        {
+            Name = string.Empty,
+            Description = string.Empty,
+        };
+        var act = () => _deviceEventService.CreateAsync(dto);
+
+        await act.ShouldThrowAsync<AbpValidationException>();
+    }
+
+    [Fact]
+    public async Task GetAsync_WithExistingId_ShouldNotBeNull()
+    {
+        var createdEntity = await CreateEntity();
+        var entity = await _deviceEventService.GetAsync(createdEntity.Id);
+
+        entity.ShouldNotBeNull();
+        entity.Name.ShouldBe(createdEntity.Name);
+        entity.Description.ShouldBe(createdEntity.Description);
+    }
+
+    [Fact]
+    public async Task GetAsync_WithNonExistentId_ShouldNotBeNull()
+    {
+        var act = () => _deviceEventService.GetAsync(Guid.Empty);
+        await act.ShouldThrowAsync<EntityNotFoundException>();
+    }
+
+    [Fact]
+    public async Task UpdateAsync_WithExistingId_ShouldUpdate()
+    {
+        const string UpdatedName = "Updated name";
+        const string UpdatedDescription = "Updated description";
+        var createdEntity = await CreateEntity();
+        var updateDto = new CreateOrUpdateDeviceEventDto
+        {
+            Name = UpdatedName,
+            Description = UpdatedDescription,
+        };
+
+        var updatedEntity = await _deviceEventService.UpdateAsync(createdEntity.Id, updateDto);
+
+        updatedEntity.Name.ShouldBe(UpdatedName);
+        updatedEntity.Description.ShouldBe(UpdatedDescription);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_WithNonExistentId_ShouldThrow()
+    {
+        var updateDto = new CreateOrUpdateDeviceEventDto
+        {
+            Name = "updated name",
+            Description = "updated description",
+        };
+        var act = () => _deviceEventService.UpdateAsync(Guid.Empty, updateDto);
+        await act.ShouldThrowAsync<EntityNotFoundException>();
+    }
+
+    [Fact]
+    public async Task Delete_WithExistingId_ShouldDelete()
+    {
+        var createdEntity = await CreateEntity();
+        await _deviceEventService.DeleteAsync(createdEntity.Id);
+    }
+
+    private async Task<DeviceEventDto> CreateEntity() =>
+        await _deviceEventService.CreateAsync(
+            new CreateOrUpdateDeviceEventDto
+            {
+                Name = _faker.Lorem.Word(),
+                Description = _faker.Lorem.Sentence(),
+            });
+}
