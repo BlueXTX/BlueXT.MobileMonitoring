fd073f75e631dda604ed0be80be0297be032a217
BlueXT <bluextchannel@gmail.com>
2023-07-17 11:03:42 +0300

    Добавил сущность DeviceEvent

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Domain/DeviceEvents/DeviceEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace BlueXT.MobileMonitoring.DeviceEvents;
+
+/// <summary>
+/// Событие устройства.
+/// </summary>
+public class DeviceEvent : FullAuditedEntity<Guid>
+{
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Описание.
+    /// </summary>
+    public string? Description { get; set; } = string.Empty;
+}
