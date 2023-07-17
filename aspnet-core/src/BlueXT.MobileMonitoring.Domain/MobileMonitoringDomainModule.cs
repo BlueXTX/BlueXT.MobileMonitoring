f20ac6c8d5fd8f6ac3b89cbfdea4610656666cbb
BlueXT <bluextchannel@gmail.com>
2023-07-17 18:14:38 +0300

    Добавил миграцию БД

--- aspnet-core/src/BlueXT.MobileMonitoring.Domain/MobileMonitoringDomainModule.cs
+++ aspnet-core/src/BlueXT.MobileMonitoring.Domain/MobileMonitoringDomainModule.cs
@@ -1,4 +1,6 @@
-﻿using Volo.Abp.AuditLogging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Volo.Abp.AuditLogging;
 using Volo.Abp.BackgroundJobs;
 using Volo.Abp.Emailing;
 using Volo.Abp.Identity;
