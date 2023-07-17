b2b9aa8a89bfc8c99d23281a57d506651a326c7f
BlueXT <bluextchannel@gmail.com>
2023-07-17 11:08:46 +0300

    Добавил DTO для DeviceEvent

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application.Contracts/DeviceEvents/CreateOrUpdateDeviceEventDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BlueXT.MobileMonitoring.DeviceEvents;
+
+/// <summary>
+/// DTO для создания или обновления DeviceEvent.
+/// </summary>
+public class CreateOrUpdateDeviceEventDto
+{
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Описание.
+    /// </summary>
+    public string? Description { get; set; } = string.Empty;
+}
---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application.Contracts/DeviceEvents/DeviceEventDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Auditing;
+
+namespace BlueXT.MobileMonitoring.DeviceEvents;
+
+/// <summary>
+/// DTO сущности DeviceEvent.
+/// </summary>
+public class DeviceEventDto : IEntityDto<Guid>, IHasCreationTime
+{
+    /// <summary>
+    /// Уникальный идентификатор.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Дата создания.
+    /// </summary>
+    public DateTime CreationTime { get; set; }
+
+    /// <summary>
+    /// Название.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Описание.
+    /// </summary>
+    public string? Description { get; set; } = string.Empty;
+}
