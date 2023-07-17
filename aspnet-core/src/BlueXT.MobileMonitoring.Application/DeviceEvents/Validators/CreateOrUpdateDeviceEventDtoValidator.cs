93f55d5f0a26d6bcbb67994fcb35aaab91997fdb
BlueXT <bluextchannel@gmail.com>
2023-07-17 11:16:12 +0300

    Добавил валидацию и конфигурацию в EF для DeviceEvent

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application/DeviceEvents/Validators/CreateOrUpdateDeviceEventDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace BlueXT.MobileMonitoring.DeviceEvents.Validators;
+
+/// <summary>
+/// Валидатор DTO <see cref="CreateOrUpdateDeviceEventDto"/>.
+/// </summary>
+public class CreateOrUpdateDeviceEventDtoValidator : AbstractValidator<CreateOrUpdateDeviceEventDto>
+{
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public CreateOrUpdateDeviceEventDtoValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(128);
+        RuleFor(x => x.Description).MaximumLength(512);
+    }
+}
---
+++ aspnet-core/src/BlueXT.MobileMonitoring.EntityFrameworkCore/Configurations/DeviceEventConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BlueXT.MobileMonitoring.DeviceEvents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+
+namespace BlueXT.MobileMonitoring.Configurations;
+
+/// <summary>
+/// Конфигурация сущности <see cref="DeviceEvent"/> в базе данных.
+/// </summary>
+public class DeviceEventConfiguration : IEntityTypeConfiguration<DeviceEvent>
+{
+    /// <summary>
+    /// Сконфигурировать сущность.
+    /// </summary>
+    /// <param name="builder">Объект предоставляющий API для конфигурации.</param>
+    public void Configure(EntityTypeBuilder<DeviceEvent> builder)
+    {
+        builder.ToTable(MobileMonitoringConsts.DbTablePrefix + nameof(DeviceEvent) + MobileMonitoringConsts.DbSchema);
+        builder.ConfigureByConvention();
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(128);
+        builder.Property(x => x.Description).HasMaxLength(512);
+    }
+}
