0149750e2ebe4b4e9ed842dc399365f3ff40788e
BlueXT <bluextchannel@gmail.com>
2023-07-18 11:31:31 +0300

    Добавил открывающуюся панель

--- angular/src/app/device-statistics/device-statistics.component.ts
+++ angular/src/app/device-statistics/device-statistics.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { DeviceStatisticDto, DeviceStatisticService } from '@proxy/device-statistics';
 import { Observable } from 'rxjs';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { DeviceEventsComponent } from '../device-events/device-events.component';
+import { SelectionType } from '@swimlane/ngx-datatable';
 
 @Component({
     selector: 'app-device-statistics',
@@ -11,10 +14,13 @@ import { Observable } from 'rxjs';
 })
 export class DeviceStatisticsComponent implements OnInit {
     deviceStatistics = { items: [], totalCount: 0 } as PagedResultDto<DeviceStatisticDto>;
+    selectionType = SelectionType.single;
+    selected: DeviceStatisticDto[] = [];
 
     constructor(
         public readonly list: ListService,
         private deviceStatisticService: DeviceStatisticService,
+        private offcanvasService: NgbOffcanvas,
     ) {}
 
     ngOnInit(): void {
@@ -24,5 +30,18 @@ export class DeviceStatisticsComponent implements OnInit {
         this.list.hookToQuery(streamCreator).subscribe(response => {
             this.deviceStatistics = response;
         });
+    }
+
+    open($event: any): void {
+        this.selected = $event.selected;
+        this.offcanvasService
+            .open(DeviceEventsComponent, {
+                position: 'end',
+                panelClass: 'w-50',
+                backdropClass: 'opacity-25',
+            })
+            .dismissed.subscribe(() => {
+                this.selected = [];
+            });
     }
 }
