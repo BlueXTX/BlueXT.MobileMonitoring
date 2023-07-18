28a4d984bcd71c511748c2403a21a84f1449d7ba
BlueXT <bluextchannel@gmail.com>
2023-07-18 12:23:17 +0300

    Добавил deviceId в query страницы

--- angular/src/app/device-statistics/device-statistics.component.ts
+++ angular/src/app/device-statistics/device-statistics.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { DeviceEventsComponent } from '../device-events/device-events.component';
 import { SelectionType } from '@swimlane/ngx-datatable';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-device-statistics',
@@ -16,24 +17,35 @@ export class DeviceStatisticsComponent implements OnInit {
     deviceStatistics = { items: [], totalCount: 0 } as PagedResultDto<DeviceStatisticDto>;
     selectionType = SelectionType.single;
     selected: DeviceStatisticDto[] = [];
+    deviceId?: string;
 
     constructor(
         public readonly list: ListService,
         private deviceStatisticService: DeviceStatisticService,
         private offcanvasService: NgbOffcanvas,
+        private readonly activeRoute: ActivatedRoute,
+        private readonly router: Router,
     ) {}
 
     ngOnInit(): void {
-        const streamCreator = (query): Observable<PagedResultDto<DeviceStatisticDto>> =>
-            this.deviceStatisticService.getList(query);
-
-        this.list.hookToQuery(streamCreator).subscribe(response => {
-            this.deviceStatistics = response;
-        });
+        this.loadQueryParams();
+        this.getDeviceStatistics();
     }
 
     open($event: any): void {
         this.selected = $event.selected;
+        this.openOffcanvas(this.selected[0].deviceId);
+    }
+
+    private loadQueryParams(): void {
+        this.deviceId = this.activeRoute.snapshot.queryParams['deviceId'];
+        if (this.deviceId !== undefined) {
+            this.openOffcanvas(this.deviceId);
+        }
+    }
+
+    private openOffcanvas(deviceId: string): void {
+        this.router.navigate([], { relativeTo: this.activeRoute, queryParams: { deviceId: deviceId } });
         this.offcanvasService
             .open(DeviceEventsComponent, {
                 position: 'end',
@@ -42,6 +54,16 @@ export class DeviceStatisticsComponent implements OnInit {
             })
             .dismissed.subscribe(() => {
                 this.selected = [];
+                this.router.navigate([], { relativeTo: this.activeRoute });
             });
+    }
+
+    private getDeviceStatistics(): void {
+        const streamCreator = (query): Observable<PagedResultDto<DeviceStatisticDto>> =>
+            this.deviceStatisticService.getList(query);
+
+        this.list.hookToQuery(streamCreator).subscribe(response => {
+            this.deviceStatistics = response;
+        });
     }
 }
