8a2d85aa76dbe83619edeb952d5aa8a15740ef17
BlueXT <bluextchannel@gmail.com>
2023-07-18 13:07:03 +0300

    Реализовал функционал выдвигающейся панели

--- angular/src/app/device-events/device-events.component.ts
+++ angular/src/app/device-events/device-events.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DeviceEventDto, DeviceEventService } from '@proxy/device-events';
+import { DeviceStatisticDto, DeviceStatisticService } from '@proxy/device-statistics';
 
 @Component({
-  selector: 'app-device-events',
-  templateUrl: './device-events.component.html',
-  styleUrls: ['./device-events.component.scss']
+    selector: 'app-device-events',
+    templateUrl: './device-events.component.html',
+    styleUrls: ['./device-events.component.scss'],
 })
-export class DeviceEventsComponent {
+export class DeviceEventsComponent implements OnInit {
+    deviceId?: string = '';
+    deviceStatistic: DeviceStatisticDto = {};
+    deviceEvents: DeviceEventDto[] = [];
 
+    constructor(
+        private readonly activeRoute: ActivatedRoute,
+        private readonly deviceStatisticService: DeviceStatisticService,
+        private readonly deviceEventsService: DeviceEventService,
+    ) {}
+
+    ngOnInit(): void {
+        this.deviceId = this.activeRoute.snapshot.queryParams['deviceId'];
+        this.deviceStatisticService.getByDeviceId(this.deviceId).subscribe(response => {
+            this.deviceStatistic = response;
+        });
+        this.deviceEventsService.getListByDeviceId(this.deviceId).subscribe(response => {
+            this.deviceEvents = response;
+        });
+    }
 }
--- angular/src/app/device-events/device-events.module.ts
+++ angular/src/app/device-events/device-events.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DeviceEventsRoutingModule } from './device-events-routing.module';
 import { DeviceEventsComponent } from './device-events.component';
-
+import { BaseThemeSharedModule } from '@abp/ng.theme.shared';
+import { LocalizationModule } from '@abp/ng.core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
-  declarations: [
-    DeviceEventsComponent
-  ],
-  imports: [
-    CommonModule,
-    DeviceEventsRoutingModule
-  ]
+    declarations: [DeviceEventsComponent],
+    imports: [CommonModule, DeviceEventsRoutingModule, BaseThemeSharedModule, LocalizationModule, NgxDatatableModule],
 })
-export class DeviceEventsModule { }
+export class DeviceEventsModule {}
