3025ffdbe4a619c5685f6380103c713941d00af8
BlueXT <bluextchannel@gmail.com>
2023-07-18 12:59:09 +0300

    Добавил возможность получения статистики по deviceId

--- aspnet-core/src/BlueXT.MobileMonitoring.Application/DeviceStatistics/DeviceStatisticService.cs
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application/DeviceStatistics/DeviceStatisticService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.ObjectMapping;
 
 namespace BlueXT.MobileMonitoring.DeviceStatistics;
 
@@ -10,12 +12,21 @@ namespace BlueXT.MobileMonitoring.DeviceStatistics;
 /// </summary>
 public class DeviceStatisticService : CrudAppService<DeviceStatistic, DeviceStatisticDto, Guid, PagedAndSortedResultRequestDto, CreateOrUpdateDeviceStatisticDto>, IDeviceStatisticService
 {
+    private readonly IObjectMapper _mapper;
+
     /// <summary>
     /// Конструктор класса.
     /// </summary>
     /// <param name="repository">Репозиторий.</param>
-    public DeviceStatisticService(IRepository<DeviceStatistic, Guid> repository)
-        : base(repository)
-    {
-    }
+    /// <param name="mapper">Преобразователь объектов.</param>
+    public DeviceStatisticService(IRepository<DeviceStatistic, Guid> repository, IObjectMapper mapper)
+        : base(repository) => _mapper = mapper;
+
+    /// <summary>
+    /// Получить <see cref="DeviceStatisticDto"/> по уникальному идентификатору устройства.
+    /// </summary>
+    /// <param name="deviceId">Уникальный идентификатор устройства.</param>
+    /// <returns>Объект <see cref="DeviceStatisticDto"/>.</returns>
+    public async Task<DeviceStatisticDto> GetByDeviceIdAsync(Guid deviceId)
+        => _mapper.Map<DeviceStatistic, DeviceStatisticDto>(await Repository.GetAsync(x => x.DeviceId == deviceId));
 }
