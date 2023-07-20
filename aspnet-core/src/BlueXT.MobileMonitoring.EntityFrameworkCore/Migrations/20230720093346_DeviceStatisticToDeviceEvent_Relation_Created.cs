f94473af604b6b2d8c27c7291fc01c2adf499a7c
BlueXT <bluextchannel@gmail.com>
2023-07-20 12:34:30 +0300

    Создал миграцию БД

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.EntityFrameworkCore/Migrations/20230720093346_DeviceStatisticToDeviceEvent_Relation_Created.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlueXT.MobileMonitoring.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeviceStatisticToDeviceEventRelationCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "device_id",
+                table: "app_device_event",
+                newName: "device_statistic_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_app_device_event_device_statistic_id",
+                table: "app_device_event",
+                column: "device_statistic_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_app_device_event_app_device_statistic_device_statistic_id",
+                table: "app_device_event",
+                column: "device_statistic_id",
+                principalTable: "app_device_statistic",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_app_device_event_app_device_statistic_device_statistic_id",
+                table: "app_device_event");
+
+            migrationBuilder.DropIndex(
+                name: "IX_app_device_event_device_statistic_id",
+                table: "app_device_event");
+
+            migrationBuilder.RenameColumn(
+                name: "device_statistic_id",
+                table: "app_device_event",
+                newName: "device_id");
+        }
+    }
+}
