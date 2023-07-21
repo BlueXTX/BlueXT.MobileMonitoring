d2db96878b6c34785d533cfe74fffd9feda9dc18
BlueXT <bluextchannel@gmail.com>
2023-07-21 15:58:58 +0300

    Создал модуль для работы с SignalR

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application.Realtime/MobileMonitoringApplicationRealtimeModule.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp.AspNetCore.SignalR;
+using Volo.Abp.Modularity;
+
+namespace BlueXT.MobileMonitoring.Application.Realtime;
+
+/// <summary>
+/// Модуль для работы обработки данных в реальном времени.
+/// </summary>
+[DependsOn(
+    typeof(MobileMonitoringDomainModule),
+    typeof(MobileMonitoringApplicationContractsModule),
+    typeof(AbpAspNetCoreSignalRModule)
+)]
+public class MobileMonitoringApplicationRealtimeModule : AbpModule
+{
+}
