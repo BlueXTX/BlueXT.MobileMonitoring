c0ea8dc18d3d1290d7d8915f87927401974c0247
BlueXT <bluextchannel@gmail.com>
2023-07-24 10:46:06 +0300

    Добавил хаб для получения DeviceStatistic

---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application.Realtime/DeviceStatisticCreatedEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using BlueXT.MobileMonitoring.Application.Realtime.Hubs;
+using BlueXT.MobileMonitoring.DeviceStatistics;
+using Microsoft.AspNetCore.SignalR;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events;
+using Volo.Abp.EventBus;
+using Volo.Abp.ObjectMapping;
+
+namespace BlueXT.MobileMonitoring.Application.Realtime;
+
+/// <summary>
+/// Обработчик события создания <see cref="DeviceStatistic"/>.
+/// </summary>
+public class DeviceStatisticCreatedEventHandler : ILocalEventHandler<EntityCreatedEventData<DeviceStatistic>>, ITransientDependency
+{
+    private readonly IHubContext<DeviceStatisticHub, IDeviceStatisticClient> _hubContext;
+    private readonly IObjectMapper _mapper;
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="hubContext">Контекст SignalR хаба.</param>
+    /// <param name="mapper">Преобразователь объектов.</param>
+    public DeviceStatisticCreatedEventHandler(IHubContext<DeviceStatisticHub, IDeviceStatisticClient> hubContext, IObjectMapper mapper)
+    {
+        _hubContext = hubContext;
+        _mapper = mapper;
+    }
+
+    /// <summary>
+    /// Обработать событие создания <see cref="DeviceStatistic"/>.
+    /// </summary>
+    /// <param name="eventData">Данные события.</param>
+    /// <returns>Задача обработки.</returns>
+    public async Task HandleEventAsync(EntityCreatedEventData<DeviceStatistic> eventData) => await _hubContext.Clients.All.ReceiveUpdate(_mapper.Map<DeviceStatistic, DeviceStatisticDto>(eventData.Entity));
+}
---
+++ aspnet-core/src/BlueXT.MobileMonitoring.Application.Realtime/Hubs/DeviceStatisticHub.cs
@@ -0,0 +1,11 @@
+﻿using BlueXT.MobileMonitoring.DeviceStatistics;
+using Volo.Abp.AspNetCore.SignalR;
+
+namespace BlueXT.MobileMonitoring.Application.Realtime.Hubs;
+
+/// <summary>
+/// Хаб для работы с <see cref="DeviceStatistic"/>.
+/// </summary>
+public class DeviceStatisticHub : AbpHub<IDeviceStatisticClient>
+{
+}
